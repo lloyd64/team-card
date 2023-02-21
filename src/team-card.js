8a4d1e7307095e32cd44f60552011029e4db9ee3
lloyd64 <122186067+lloyd64@users.noreply.github.com>
2023-02-21 12:15:10 -0500

    duplicate/delete

--- src/team-card.js
+++ src/team-card.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 
-import "@lrnwebcomponents/meme-maker/meme-maker.js";
+//import "@lrnwebcomponents/meme-maker/meme-maker.js";
 
 
 const bryce = new URL("https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2022/10/1440/810/bryce-hr.jpg?ve=1&tl=1", import.meta.url).href;
